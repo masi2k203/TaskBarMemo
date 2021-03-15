e536da8126769b51a7c4000d5706ea262189fc32
mas2kf3 <moto20000203@gmail.com>
2021-03-15 21:07:59 +0900

    メモ削除機能追加 暫定リリース版

---
+++ TaskBarMemo/ViewModels/SettingPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Reactive.Bindings;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace TaskBarMemo.ViewModels
+{
+    class SettingPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region プロパティ
+
+        /// <summary>
+        /// アセンブリファイルバージョンのプロパティ
+        /// </summary>
+        public ReactiveProperty<string> FileVersionProperty { get; set; } = new();
+        /// <summary>
+        /// 追加のバージョンプロパティ（非常用・Devチャネルのみ）
+        /// </summary>
+        public ReactiveProperty<string> AdditionalVersionProperty { get; set; } = new("_alpha1_release_dev");
+
+        #endregion
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public SettingPageViewModel()
+        {
+            // アセンブリファイルバージョンを取得する
+            var assembly = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
+            FileVersionProperty.Value = assembly.FileVersion + AdditionalVersionProperty.Value;
+        }
+
+
+        #region メソッド
+
+        #endregion
+    }
+}
